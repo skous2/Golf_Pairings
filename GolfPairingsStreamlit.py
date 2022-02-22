b4a442b53d4334c7c3e8d13b3bd192e07ed1dec3
skous2 <tanner.skousen@gmail.com>
2022-02-21 21:26:28 -0500

    Update GolfPairingsStreamlit.py

--- GolfPairingsStreamlit.py
+++ GolfPairingsStreamlit.py
@@ -135,5 +135,6 @@ try:
         #with open(path, 'w') as f:
             #print('Day1:\n' + day1.to_string() + '\n\n\nDay2:\n' + day2.to_string() + '\n\n\nDay3:\n' + day3.to_string(), file=f) 
     st.write('Note: clicking this will reshuffle the list if that option is checked')
+    st.write('=OFFSET($A$1,COLUMNS($A1:A1)-1+(ROWS($1:1)-1)*4,0)')
 except:
     st.write("The current configuration of the parameters does not yield a valid result")
